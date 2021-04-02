6118497604550baa14ecb9057828c99be73819c8
D7K7 <j3susf1shy@gmail.com>
2021-04-01 22:10:19 -0500

    Added cs file.

---
+++ Find-The-Parity-Outlier.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+
+public static void Main()
+  {
+    int[] testArray = {2, 4, 0, 100, 4, 11, 2602, 36};
+    Console.WriteLine(Find(testArray));
+  }
+
+  public static int Find(int[] integers)
+  {
+    return (integers.Where(x => x % 2 != 0).Count() == 1) ? 
+    integers.Where(x => x % 2 != 0).First() : integers.Where(x => x % 2 == 0).First();
+  
+  }
+}
